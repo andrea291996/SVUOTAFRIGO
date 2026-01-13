9604adb77dfc2f3f8297a8b40527c1b246406a2c
Andrea Carminati <carminati.andrea96@gmail.com>
2026-01-13 16:28:53 +0100

    fix: nome classe nel file assistenzacontroller e bug minori

--- controllers/AssistenzaController.php
+++ controllers/AssistenzaController.php
@@ -3,12 +3,12 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class ContattiController extends Controller {
+class AssistenzaController extends Controller {
 
     public function assistenza(Request $request, Response $response, $args){
         $page = PageConfigurator::instance()->getPage(); 
         $page->setTitle("Assistenza");
-        $page->add("content", new PageElement());
+        $page->add("content", new PageElement("ui/assistenza"));
 
         return $response;
     }
