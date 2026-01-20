6f45ddf2f4e51edb5aa5e634e4918791c6653cf1
Andrea Carminati <carminati.andrea96@gmail.com>
2026-01-20 15:03:52 +0100

    ora ogni card ricetta ha l'id della ricetta

--- models/Ricetta.php
+++ models/Ricetta.php
@@ -7,13 +7,15 @@ class Ricetta {
     protected $testoDieta;
     protected $tipologia;
     protected $template;
+    protected $idRicetta;
 
-    function __construct($titolo, $procedimento, $ingredienti, $testoDieta, $tipologia, $template = "ricette/ricetta"){
+    function __construct($titolo, $procedimento, $ingredienti, $testoDieta, $tipologia, $idRicetta, $template = "ricette/ricetta"){
         $this->titolo = $titolo;
         $this->procedimento = $procedimento;
         $this->ingredienti = $ingredienti;
         $this->testoDieta = $testoDieta;
         $this->tipologia = $tipologia;
+        $this->idRicetta = $idRicetta;
     }
 
     public function getTitolo() { return $this->titolo; }
@@ -21,4 +23,5 @@ class Ricetta {
     public function getIngredienti() { return $this->ingredienti; }
     public function getTestoDieta() { return $this->testoDieta; }
     public function getTipologia() { return $this->tipologia; }
+    public function getIdRicetta() { return $this->idRicetta; }
 }
